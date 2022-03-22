07bda63c139e1c33ac970d99db964e57015c8849
tyange <usun16@gmail.com>
2022-03-22 09:51:53 +0900

    기본적인 사이드바 만들기

---
+++ components/side-bar.tsx
@@ -0,0 +1,32 @@
+const SideBar = () => {
+  return (
+    <aside>
+      <div>
+        <div>
+          <ul>
+            <li>ingredients 1</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>ingredients 1</li>
+            <li>ingredients 2</li>
+            <li>ingredients 3</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>ingredients 4</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>ingredients 5</li>
+          </ul>
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default SideBar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import SideBar from "../components/side-bar";
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +11,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <p className="text-amber-600">next burger finder</p>
+        <SideBar />
       </main>
     </div>
   );
