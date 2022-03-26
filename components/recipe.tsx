9d765a66bff7d89dba5206ef75f6aa834b3db308
tyange <usun16@gmail.com>
2022-03-27 08:51:38 +0900

    패딩 써서 안쪽으로 조금 들여쓰기

--- components/recipe.tsx
+++ components/recipe.tsx
@@ -15,12 +15,12 @@ const Recipe = () => {
   return (
     <div className="box-border flex flex-col col-span-5 p-5 border shadow-md rounded-xl">
       <div className="flex flex-col items-end flex-1">
-        <ul>
+        <ul className="pr-3">
           {data.ingredients.map(
             (ingredient) =>
               ingredient.amount !== 0 && (
                 <li key={ingredient.name}>
-                  <span>{ingredient.name}</span>
+                  <span>{nameConverter(ingredient.name)}</span>
                   <span>{ingredient.amount}</span>
                 </li>
               )
@@ -29,7 +29,7 @@ const Recipe = () => {
       </div>
       <div className="box-border flex justify-end pt-3 border-t-2">
         <span className="mr-3">선택한 재료의 총 개수 :</span>
-        <span>
+        <span className="pr-3">
           {data.ingredients.reduce((prev, next) => prev + next.amount, 0)}
         </span>
       </div>
